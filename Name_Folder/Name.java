67d4a42218f2cc0202091ea9a137fe478731447e
ajani35 <ajanidawson69@gmail.com>
2023-04-14 09:35:22 -0500

    Homework_Turn_In

---
+++ Name_Folder/Name.java
@@ -0,0 +1,9 @@
+public class Name {
+	
+	public static void main(String[] args) {
+		String name = "Ajani.";
+		String message = " Welcome to Solvd!!!";
+		System.out.println("Hello " + name  + message);
+	}
+
+}
